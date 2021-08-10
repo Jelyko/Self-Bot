23175c203d510f10bcdaadbf5895e21cbe409e78
Jelyko TCG <jasper.demmers@hotmail.com>
2021-08-10 16:55:12 +0200

    Updated with try()

--- index.js
+++ index.js
@@ -8,30 +8,37 @@ require('dotenv').config();
 client.on("ready", async() => {
     console.log(`${client.user.tag} has logged in`)
     client.user.setActivity("Grinding Levels", {type: "PLAYING"})
+    try {
+        const Guild = client.guilds.cache.get(process.env.GUILD)
+        const Channel = Guild.channels.cache.get(process.env.CHANNEL)
+        
+        const choices = [
+            "Kunwar will lose",
+            "Kunwar wont win",
+            "Ima get this number one",
+            "apple",
+            "apple pie",
+            "joink",
+            "lv69 max is a legend",
+            "killer is best",
+            "I coded this bot",
+            "You dont know who I am",
+            "I am disquised as a lebanese",
+            "I did not get cwl bonus :( but I dont care",
+            "Can I get your number?",
+            "Get it",
+            "hiv",
+            "zwerkoe",
+            "coc botter",
+        ];
+    
+        setInterval(() => {
+            Channel.send(`${choices[Math.floor(Math.random() * 16)]}`)
+        }, 30000);
 
-    const Guild = client.guilds.cache.get(process.env.GUILD)
-    const Channel = Guild.channels.cache.get(process.env.CHANNEL)
-
-    const lmao = [
-        "Kunwar will lose",
-        "Kunwar wont win",
-        "Ima get this number one",
-        "apple",
-        "apple pie",
-        "joink",
-        "lv69 max is a legend",
-        "killer is best",
-        "I coded this bot",
-        "You dont know who I am",
-        "I am disquised as a lebanese",
-        "I did not get cwl bonus :( but I dont care",
-        "Can I get your number?",
-        "Get it",
-    ];
-
-    setInterval(() => {
-        Channel.send(`${lmao[Math.floor(Math.random() * 11)]}`)
-    }, 30000);
+    } catch(err) {
+        console.log(err)
+    }
 })
 
 client.login(process.env.TOKEN);
